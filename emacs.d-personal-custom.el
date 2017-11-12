6178c06d0cc244e3f192fded489c417d196e3ac0
Johannes Choo <jhanschoo@gmail.com>
2017-11-13 00:02:59 +0800

    use iosevka font for emacs

--- emacs.d-personal-custom.el
+++ emacs.d-personal-custom.el
@@ -15,5 +15,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Inziu Iosevka SC"))))
- '(fixed-pitch ((t (:family "Inziu Iosevka SC")))))
+ '(default ((t (:family "Iosevka"))))
+ '(fixed-pitch ((t (:family "Iosevka")))))
